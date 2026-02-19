cbf21d211413746b317994b490c2eed635170d80
joshalvinronquillo <j26ronquillo@gmail.com>
2026-02-18 18:54:12 -0800

    found the project theme and created the costum class with one string, int, and bool field.

---
+++ COMP003A.Assignment6/COMP003A.Assignment6/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace COMP003A.Assignment6
+{
+    class WorkoutSession
+    {
+        //Theme B Fitness Session Planner
+
+            public string Name;
+            public int Age;
+            public bool Weight;
+
+            public WorkoutSession(string name, int age, bool weight)
+            {
+                Name = name;
+                Age = age;
+                Weight = weight;
+            }
+        
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
