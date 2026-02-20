03793b8e9a935ffccf05d21e3f6785f7adf89248
j26ronquillo <joshalvinpogi26@yahoo.com>
2026-02-19 18:57:02 -0800

    finished switch command for case 2 and finished the switch command for whole option

--- COMP003A.Assignment6/COMP003A.Assignment6/Program.cs
+++ COMP003A.Assignment6/COMP003A.Assignment6/Program.cs
@@ -18,8 +18,8 @@
         public void DisplayInfo()
         {
             Console.Write($"Name: " + Name);
-			Console.Write($"Name: " + Duration + "minutes");
-			Console.Write($"Completed: " + Workout);
+			Console.Write($"Workout: " + Duration + " minutes. ");
+			Console.Write($"Completed: " + Workout + "\n");
 		} 
 
         public void FinishWorkout()
@@ -30,22 +30,58 @@
         static void Main(string[] args)
         {
 
-            WorkoutSession sessionone = new WorkoutSession("Cardio", 30, false);
-            WorkoutSession sessiontwo = new WorkoutSession("Sit-ups till failure", 30, false);
-            WorkoutSession sessionthree = new WorkoutSession("Push-ups till failure", 30, false);
+            WorkoutSession SessionOne = new WorkoutSession("Cardio. ", 30, false);
+            WorkoutSession SessionTwo = new WorkoutSession("Sit-ups till failure. ", 30, false);
+            WorkoutSession SessionThree = new WorkoutSession("Push-ups till failure. ", 30, false);
 
-            {
-                Console.WriteLine("1. Display All\n2. Finished a Workout\n3. Exit Program");
-            }
+			Console.WriteLine("1. Display All\n2. Finished a Workout\n3. Exit Program");
+			int choice = 0;
 
-            int choice = 0;
-            while (choice != 1)
+            while (choice != 3)
             {
-                switch (choice) {
+                Console.Write("\nEnter Choice: ");
+				choice = int.Parse(Console.ReadLine());
+
+				switch (choice)
+                {
                     case 1:
+                        Console.WriteLine("\nSession 1:");
+                        SessionOne.DisplayInfo();
 
+                        Console.WriteLine("\nSession 2:");
+                        SessionTwo.DisplayInfo();
+
+                        Console.WriteLine("\nSession 3:");
+                        SessionThree.DisplayInfo();
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Select session to update (1-3): ");
+                        int secondChoice = int.Parse(Console.ReadLine());
+
+                        switch (secondChoice) 
+                        {
+                            case 1:
+                                SessionOne.FinishWorkout();
+                                Console.WriteLine("Session 1 updated.");
+                                break;
+							case 2:
+								SessionTwo.FinishWorkout();
+								Console.WriteLine("Session 2 updated.");
+								break;
+							case 3:
+								SessionThree.FinishWorkout();
+								Console.WriteLine("Session 3 updated.");
+								break;
+                            default:
+                                Console.WriteLine("ERROR: Invalid Option");
+                                break;
+						}
+                        break;
+
+                    case 3:
+                        Console.WriteLine(""
             }
-
         }
     }
 }
