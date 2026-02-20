e4723b4ce89b52ef3f432658088976b1e0b8fc66
j26ronquillo <joshalvinpogi26@yahoo.com>
2026-02-19 18:17:06 -0800

    fixed errors and added proper classes and fields. working on choices and "switch" command

--- COMP003A.Assignment6/COMP003A.Assignment6/Program.cs
+++ COMP003A.Assignment6/COMP003A.Assignment6/Program.cs
@@ -5,24 +5,47 @@
         //Theme B Fitness Session Planner
 
             public string Name;
-            public int Age;
+            public int Duration;
             public bool Workout;
 
-            public WorkoutSession(string name, int age, bool workout)
+            public WorkoutSession(string name, int duration, bool workout)
             {
                 Name = name;
-                Age = age;
+                Duration = duration;
                 Workout = workout;
             }
 
         public void DisplayInfo()
         {
-            Console.WriteLine($""
-        } 
+            Console.Write($"Name: " + Name);
+			Console.Write($"Name: " + Duration + "minutes");
+			Console.Write($"Completed: " + Workout);
+		} 
+
+        public void FinishWorkout()
+        {
+            Workout = true;
+        }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("1. Display All\n2. Finished a Workout\n3. Exit Program");
+
+            WorkoutSession sessionone = new WorkoutSession("Cardio", 30, false);
+            WorkoutSession sessiontwo = new WorkoutSession("Sit-ups till failure", 30, false);
+            WorkoutSession sessionthree = new WorkoutSession("Push-ups till failure", 30, false);
+
+            {
+                Console.WriteLine("1. Display All\n2. Finished a Workout\n3. Exit Program");
+            }
+
+            int choice = 0;
+            while (choice != 1)
+            {
+                switch (choice) {
+                    case 1:
+
+            }
+
         }
     }
 }
